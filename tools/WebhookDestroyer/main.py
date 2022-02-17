c66fcc0fd6aec2653265e067ebc361e7391125f9
echqq00 <99441350+Echqq00@users.noreply.github.com>
2022-02-17 22:52:11 +0100

    Delete tools/WebhookDestroyer directory

--- tools/WebhookDestroyer/main.py
+++
@@ -1,8 +0,0 @@
-import tools.WebhookDestroyer.utilities.name as name
-import tools.WebhookDestroyer.utilities.webhook as webhook
-import tools.WebhookDestroyer.utilities.deleteWebhook as delhook
-
-def start():
-    name.main()
-    webhook.main()
-    delhook.main()
